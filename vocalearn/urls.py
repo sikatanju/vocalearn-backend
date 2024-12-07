90183dd8ef22abaa420a6d8fc7e7a6a2b955094d
sikatanju <saadtanju@gmail.com>
2024-12-07 13:02:49 +0600

    Refactor- centralize speech-to-text view

--- vocalearn/urls.py
+++ vocalearn/urls.py
@@ -5,10 +5,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
     path("translate/", views.translate_text_view, name='translate_text'),
     path("speech/", views.speech_to_text_view, name="speech-to-text"),
-    path("record/", views.recorded_audio, name='recorded_audio'),
 ]
 
 if settings.DEBUG:
