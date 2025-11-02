6b562e0d6ec8e328909c3abc8ed06753a729225d
sayad <sayad.subscriptionpro@gmail.com>
2025-11-02 21:04:45 +0600

    setup djoser and jwt for authentication

--- core/serializers.py
+++ core/serializers.py
@@ -3,8 +3,8 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer,
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'mobile']
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'mobile']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
